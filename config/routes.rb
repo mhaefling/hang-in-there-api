f053370411b21de012e7f59ed2c8873634f45b6f
Jonathan Atkins <jonathanjatkins89@gmail.com>
2024-12-19 10:29:36 -0800

    feat:updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
   post "/api/v1/posters",          to: "api/v1/posters#create"
   patch "/api/v1/posters/:id",     to: "api/v1/posters#update"
   delete "/api/v1/posters/:id",    to: "api/v1/posters#destroy"
-  get "/api/v1/posters",           to: "api/v1/posters#sort"
 end
